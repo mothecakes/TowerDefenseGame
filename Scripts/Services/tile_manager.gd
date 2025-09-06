1e5b781b4345899bc23d1d7ff1684f51954af9e6
Joseph Sary <63524400+mothecakes@users.noreply.github.com>
2025-09-06 00:01:08 -0700

    implemented working pathfinding, alongside unit ai navigation waypoints

--- Scripts/Services/tile_manager.gd
+++ Scripts/Services/tile_manager.gd
@@ -1,54 +1,87 @@
 class_name TileManager
 extends Node2D
 
-@export var ground : TileMapLayer # navigatable terrain
+@export var ground_layer : TileMapLayer # navigatable terrain
 
-@export var wall : TileMapLayer		# defines bounds and solid tiles
-@export var high_ground : TileMapLayer
+@export var wall_layer : TileMapLayer		# defines bounds and solid tiles
+@export var high_ground_layer : TileMapLayer
 
-@export var end : TileMapLayer #defines where to end per level
-var end_position : Vector2i
+@export var spawn_layer : TileMapLayer
+@export var exit_layer : TileMapLayer #defines where to exit per level
+#var exit_position : Vector2i
+
+var spawn_positions : Array[Vector2i] 
+var exit_positions : Array[Vector2i] 
 
 var astar : AStarGrid2D = AStarGrid2D.new()
-var cell_size : float = 32
+var cell_size : Vector2 = Vector2(32,32)
 
 
 func _ready() -> void:
 	# initial bound setup
-	var rect = ground.get_used_rect()
-	astar.size = rect.size
-	astar.region = rect
-	astar.cell_size = Vector2(cell_size, cell_size)
+	#var rect = ground_layer.get_used_rect()
+	#astar.region = rect
+	astar.cell_size = cell_size
+	astar.offset = astar.cell_size / 2
 	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
-	astar.update()
+	#astar.update()
+	"""
 
 	# setup solid tiles from walls
-	var wall_tiles : Array[Vector2i] = wall.get_used_cells_by_id()
-	print(wall_tiles)
+	var wall_tiles : Array[Vector2i] = wall_layer.get_used_cells_by_id()
 	for pos : Vector2i in wall_tiles: 
 		astar.set_point_solid(pos - rect.position)
 
-	# setup solid tiles from high ground
-	var high_ground_tiles : Array[Vector2i] = high_ground.get_used_cells_by_id()
-	print(high_ground_tiles)
+	# setup solid tiles from high ground_layer
+	var high_ground_tiles : Array[Vector2i] = high_ground_layer.get_used_cells_by_id()
 	for pos : Vector2i in high_ground_tiles: 
 		astar.set_point_solid(pos - rect.position)
 
-	# setup end tile
-	var end_tiles : Array[Vector2i] = end.get_used_cells_by_id()
-	print("end: ", end_tiles)
+	# setup exit tile
+	var end_tiles : Array[Vector2i] = exit.get_used_cells_by_id()
 	for pos : Vector2i in end_tiles: #should only have 1
-		end_position = pos
+		exit_position = pos
+	Events.emit_signal("room_generated")
+	"""
+	generate_room(ground_layer, spawn_layer, exit_layer, [wall_layer, high_ground_layer])
 
-func generate_waypoints(start_pos : Vector2i):
-	var start_id = start_pos - ground.get_used_rect().position
-	print(astar.get_id_path(start_id,end_position))
-	var path := astar.get_id_path(start_id, end_position)
+func generate_room(ground : TileMapLayer, spawn : TileMapLayer, exit : TileMapLayer, \
+	solid : Array[TileMapLayer] ):
+	# generate an empty navigatable area
+	astar.region = ground.get_used_rect()
+	astar.update()
+
+	# populate region with solid tiles
+	for tile_map : TileMapLayer in solid:
+		var tiles : Array[Vector2i] = tile_map.get_used_cells()
+		for pos : Vector2i in tiles:
+			astar.set_point_solid(pos, true)
+
+	# populate list of exit positions for other game objects to optionally use
+	exit_positions.clear()
+	var exit_tiles : Array[Vector2i] = exit.get_used_cells()
+	for pos : Vector2i in exit_tiles:
+		if not astar.is_point_solid(pos):
+			exit_positions.append(pos)
+
+	spawn_positions.clear()
+	var spawn_tiles : Array[Vector2i] = spawn.get_used_cells()
+	for pos : Vector2i in spawn_tiles:
+		if not astar.is_point_solid(pos):
+			spawn_positions.append(pos)
+
+	Events.emit_signal("room_generated")
+
+func generate_waypoints(start_pos : Vector2i, exit_pos : Vector2i):
+	var start_id = start_pos - ground_layer.get_used_rect().position
+	var path : PackedVector2Array = astar.get_point_path(start_id, exit_pos)
 	return path
 
 # used to find alternate path
 # TODO : implement temporary solid tiles
+"""
 func update_waypoints(local_pos : Vector2):
-	var local_to_id = ground.local_to_map(local_pos)
-	var path : PackedVector2Array = astar.get_point_path(local_to_id, end_position)
+	var local_to_id = ground_layer.local_to_map(local_pos)
+	var path : PackedVector2Array = astar.get_point_path(local_to_id, exit_position)
 	return path
+"""
--- Scripts/Units/Enemies/Snecko.gd
+++ Scripts/Units/Enemies/Snecko.gd
@@ -1,48 +1,66 @@
 extends CharacterBody2D
 
-@export var speed : float = 5.0
-
 @export var tile_manager : TileManager
-
 @export var ground : TileMapLayer
 
-var waypoints : Array[Vector2]
+@export var min_distance : float = 0.05
+@export var speed : float = 5.0
+
+# waypoints
+var waypoints : PackedVector2Array
 var waypoint_index : int = 0
-var min_distance : float = 0.05
+var spawn_positions : Array[Vector2i]
+var exit_positions : Array[Vector2i]
 
-# supplied by spawner
-# and updated once waypoint is reached
-var pos : Vector2i
-
+# movement related vars
+var grid_position : Vector2i # grid position instead of pixel position
+var exit_position : Vector2i
 var move_direction : Vector2i
-
-var move_time_max := 0.5
-var move_time := 0.0
+var destination_reached : bool = false
 
 func _ready() -> void:
-	pos = ground.local_to_map(position)
-	var waypoints_int : Array[Vector2i] = tile_manager.generate_waypoints(pos).duplicate()
-	for i in range(waypoints_int.size()):
-		waypoints.append(ground.map_to_local(waypoints_int[i] as Vector2))
-	print(waypoints)
+	#grid_position = ground.local_to_map(position)
+	await Events.room_generated 
+	exit_positions =  tile_manager.exit_positions.duplicate()
+	spawn_positions = tile_manager.spawn_positions.duplicate()
+	if spawn_positions.size() != 0:
+		grid_position = spawn_positions.pick_random()
+		position = ground.map_to_local(grid_position)
+	else:
+		print("no valid spawn position")
+		return
+	if exit_positions.size() != 0:
+		exit_position = exit_positions.pick_random()
+	else:
+		print("no valid exit position")
+		return
 
-func _physics_process(delta: float) -> void:
-	var direction : Vector2 = (waypoints[0] - position).normalized()
-	velocity = direction * speed * delta
+	waypoints = tile_manager.generate_waypoints(grid_position, exit_positions[0]).duplicate()
 
-	check_waypoint_reached()
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2.ZERO
+	if not destination_reached:
+		var direction : Vector2 = (waypoints[waypoint_index] - position).normalized()
+		print("waypoint: ", waypoints[waypoint_index])
+		print("position: ", position)
+		velocity = direction * speed 
 
+		check_waypoint_reached()
+	move_and_slide()
+
+# update position and waypoint index if close enough to waypoint
 func check_waypoint_reached():
 	if _is_near_waypoint():
 		var waypoint = waypoints[waypoint_index]
-		pos = ground.local_to_map(waypoint)
-		waypoint_index += 1
-	pass
+		grid_position = ground.local_to_map(waypoint)
 
-# update map position and pop from waypoint stack
+		waypoint_index += 1
+		if waypoint_index >= waypoints.size():
+			destination_reached = true
+
+# checking if position is near waypoint
 func _is_near_waypoint():
 	var waypoint = waypoints[waypoint_index]
-	if position.x <= waypoint.x + min_distance and position.x >= waypoint.x - min_distance and \
-		position.y <= waypoint.y + min_distance and position.y >= waypoint.y - min_distance:
-			return true
+	if position.distance_to(waypoint) <= min_distance:
+		return true
 	return false
